953cfa5acab299041b89c00c6ca3f834e67a5dff
megli2 <egli@puzzle.ch>
2024-12-11 13:51:02 +0100

    Extract code of avg ch and dch into single method which returns average of custom defined attribute value

--- analytics_battery_2.0.py
+++ analytics_battery_2.0.py
@@ -47,51 +47,38 @@ def reduced_dataframe(battery_dataframe, cycles):
         else:    
             reduced_dataframe = reduced_dataframe[reduced_dataframe['Instruction Name.' + str(index)] != 'Idle']
             reduced_dataframe = reduced_dataframe[reduced_dataframe['Cycle.' + str(index)].isin(cycles)]
-    return reduced_dataframe      
+    return reduced_dataframe   
 
-def calculate_avg_charge(cell_dataframes, cycles):
+def calculate_avg_value(cell_dataframes, cycles, instruction_name, attribute_name):
     cell_sum = []
     for cell_index, cell in enumerate(cell_dataframes):
         cycle_sum = []
         for cycle in cycles:
             significant_data = cell
             if cell_index == 0:
-                significant_data = cell[cell['Instruction Name'] == 'V Charge']
+                significant_data = cell[cell['Instruction Name'] == instruction_name]
                 significant_data = significant_data[significant_data['Cycle'] == cycle]
-                cycle_sum.append(significant_data['Charge Capacity (mAh)'].max())
+                cycle_sum.append(significant_data[attribute_name].max())
             else:
-                significant_data = cell[cell['Instruction Name.' + str(cell_index)] == 'V Charge']
+                significant_data = cell[cell['Instruction Name.' + str(cell_index)] == instruction_name]
                 significant_data = significant_data[significant_data['Cycle.' + str(cell_index)] == cycle]
-                cycle_sum.append(significant_data['Charge Capacity (mAh).' + str(cell_index)].max())
+                cycle_sum.append(significant_data[attribute_name + '.' + str(cell_index)].max())
         cell_sum.append(st.mean(cycle_sum))  
     return st.mean(cell_sum)
 
-
-def calculate_avg_discharge(cell_dataframes, cycles):
-    cell_sum = []
-    for cell_index, cell in enumerate(cell_dataframes):
-        cycle_sum = []
-        for cycle in cycles:
-            significant_data = cell
-            if cell_index == 0:
-                significant_data = cell[cell['Instruction Name'] == 'I Disch.']
-                significant_data = significant_data[significant_data['Cycle'] == cycle]
-                cycle_sum.append(significant_data['Discharge Capacity (mAh)'].max())
-            else:
-                significant_data = cell[cell['Instruction Name.' + str(cell_index)] == 'I Disch.']
-                significant_data = significant_data[significant_data['Cycle.' + str(cell_index)] == cycle]
-                cycle_sum.append(significant_data['Discharge Capacity (mAh).' + str(cell_index)].max())
-        cell_sum.append(st.mean(cycle_sum))  
-    return st.mean(cell_sum) 
-
+# Define file to analyse and relevant cycles in it
 file_path = '/home/megli2/Downloads/24-003_PhysicalTwin_Diagnostic_Test14525.csv'
 cycles = [2., 3., 4.]
-battery_dataframe = pd.read_csv(file_path)
 
+# Read csv data into pandas dataframe and reduce it to the needed values
+battery_dataframe = pd.read_csv(file_path)
 reduced_dataframe_battery = reduced_dataframe(battery_dataframe, cycles)
+
+# Split dataframe into dataframes per cell
 cell_dataframes = get_cell_dataframes(battery_dataframe)
 
-average_charge = calculate_avg_charge(cell_dataframes, cycles)
-average_discharge = calculate_avg_discharge(cell_dataframes, cycles)
+# Calculate charge from each cell and print results
+average_charge = calculate_avg_value(cell_dataframes, cycles, 'V Charge', 'Charge Capacity (mAh)')
+average_discharge = calculate_avg_value(cell_dataframes, cycles, 'I Disch.', 'Discharge Capacity (mAh)')
 print("Average Charge: " + str(average_charge) + " mAh")
 print("Average Discharge: " + str(average_discharge) + " mAh")
