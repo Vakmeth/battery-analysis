b82182cbbe1a06026e091448ce69235abbf005b6
megli2 <egli@puzzle.ch>
2025-01-02 15:38:04 +0100

    feat(SOC): implement soc diagram over all three cycles

--- diagnostic_results.py
+++ diagnostic_results.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import ch_dch_plot as cdp
 import matplotlib.pyplot as plt
+import statistics as st
 
 # Define file to analyse and read out dataframe from it
 file_path_cell_1 = 'data_export/Cell2_Diagnostic_Test14893.csv'
@@ -12,14 +13,17 @@ cell_dataframe_3 = pd.read_csv(file_path_cell_3)
 
 cell_dataframes = [cell_dataframe_1, cell_dataframe_2]
 
-# Plot ch/dch ploting
+# Define analyzed cycles
+CYCLES = [2, 3, 4]
+
+# CH / DCH 
+# -------------------------------------------------- #
 fig, ax = plt.subplots(figsize=(5, 2.7))
 ax.set_xlabel("Time")
 ax.set_ylabel("Capacity")
 
 for cell_dataframe in cell_dataframes:
-    cycles = [ 2, 3, 4 ]
-    for cycle in cycles:
+    for cycle in CYCLES:
         data = cdp.plot_ch_dch(cell_dataframe, cycle) 
 
         # Plot CH and DCH figure for this cycle
@@ -27,4 +31,36 @@ for cell_dataframe in cell_dataframes:
         ax.plot(data["dch"]["time_points"], data["dch"]["capacity_points"], color='green')
 
 # Show plotted figure
+# plt.show()
+
+# -------------------------------------------------- # 
+
+
+# SOC 
+# -------------------------------------------------- #
+fig2, ax2 = plt.subplots(figsize=(5, 2.7))
+ax2.set_xlabel("Time")
+ax2.set_ylabel("Capacity")
+
+for cycle in CYCLES:
+    # Filter data
+    filtered_frame = cell_dataframe_2[cell_dataframe_2['Cycle'] == cycle]
+    soc_dataframe = filtered_frame[filtered_frame['Step'].isin([21, 23])]
+    INITIAL_SOC = 0
+
+    # Integrate current
+    current = soc_dataframe['Current (mA)']
+    DELTA_T = 1 / 3600
+    # Calculate total charge using numerical integration
+    total_charge = sum(I * DELTA_T for I in current)
+
+    # Calculate nominal capacity
+    nominal_capacity = soc_dataframe['Charge Capacity (mAh)'].mean()
+    soc_values = [0]
+
+    for row in soc_dataframe.iterrows():
+        soc_values.append(INITIAL_SOC + total_charge / nominal_capacity)
+        INITIAL_SOC = soc_values[-1]
+
+    ax2.plot(list(range(0, len(soc_values))), soc_values, color='purple')
 plt.show()
