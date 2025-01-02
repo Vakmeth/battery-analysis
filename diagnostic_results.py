00adeb83dc3090d627a1bd739e681d5fd1c5d9bc
megli2 <egli@puzzle.ch>
2025-01-02 16:03:09 +0100

    feat(SOC): implement soc diagram for all cells

--- diagnostic_results.py
+++ diagnostic_results.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import ch_dch_plot as cdp
 import matplotlib.pyplot as plt
-import statistics as st
+import random
 
 # Define file to analyse and read out dataframe from it
 file_path_cell_1 = 'data_export/Cell2_Diagnostic_Test14893.csv'
@@ -25,7 +25,7 @@ ax.set_ylabel("Capacity")
 for cell_dataframe in cell_dataframes:
     for cycle in CYCLES:
         data = cdp.plot_ch_dch(cell_dataframe, cycle) 
-
+        
         # Plot CH and DCH figure for this cycle
         ax.plot(data["ch"]["time_points"], data["ch"]["capacity_points"], color='blue')
         ax.plot(data["dch"]["time_points"], data["dch"]["capacity_points"], color='green')
@@ -42,25 +42,30 @@ fig2, ax2 = plt.subplots(figsize=(5, 2.7))
 ax2.set_xlabel("Time")
 ax2.set_ylabel("Capacity")
 
-for cycle in CYCLES:
-    # Filter data
-    filtered_frame = cell_dataframe_2[cell_dataframe_2['Cycle'] == cycle]
-    soc_dataframe = filtered_frame[filtered_frame['Step'].isin([21, 23])]
-    INITIAL_SOC = 0
+for cell_dataframe in cell_dataframes:
+    random_color = (random.random(), random.random(), random.random())
+    for cycle in CYCLES:
+        # Filter data
+        filtered_frame = cell_dataframe[cell_dataframe['Cycle'] == cycle]
+        soc_dataframe = filtered_frame[filtered_frame['Step'].isin([21, 23])]
+        INITIAL_SOC = 0
 
-    # Integrate current
-    current = soc_dataframe['Current (mA)']
-    DELTA_T = 1 / 3600
-    # Calculate total charge using numerical integration
-    total_charge = sum(I * DELTA_T for I in current)
+        # Integrate current
+        current = soc_dataframe['Current (mA)']
+        DELTA_T = 1 / 3600
+        # Calculate total charge using numerical integration
+        total_charge = sum(I * DELTA_T for I in current)
 
-    # Calculate nominal capacity
-    nominal_capacity = soc_dataframe['Charge Capacity (mAh)'].mean()
-    soc_values = [0]
+        # Calculate nominal capacity
+        nominal_capacity = soc_dataframe['Charge Capacity (mAh)'].mean()
+        soc_values = []
 
-    for row in soc_dataframe.iterrows():
-        soc_values.append(INITIAL_SOC + total_charge / nominal_capacity)
-        INITIAL_SOC = soc_values[-1]
+        for row in soc_dataframe.iterrows():
+            soc_values.append(INITIAL_SOC + total_charge / nominal_capacity)
+            INITIAL_SOC = soc_values[-1]
+        
+        plt.plot(soc_dataframe['Total Time (Seconds)'], soc_values, 
+                label='State of Charge (SOC)', color=random_color)
 
-    ax2.plot(list(range(0, len(soc_values))), soc_values, color='purple')
 plt.show()
+plt.legend()
